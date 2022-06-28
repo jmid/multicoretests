c2a743c0b3ba2b3b6776a85b23945432a7afe5f7
n-osborne <nicolas.osborne@tarides.com>
2022-06-28 09:57:47 +0200

    split ref_stm_tests in three files

--- src/neg_tests/ref_stm_spec.ml
+++ src/neg_tests/ref_stm_spec.ml
@@ -132,29 +132,3 @@ struct
     | Decr,  Res ((Unit,_),_) -> true
     | _,_ -> false
 end
-
-
-module RT_int_Seq   = STM_Seq.Make(RConf_int)
-module RT_int_Dom   = STM_Domain.Make(RConf_int)
-module RT_int64_Seq = STM_Seq.Make(RConf_int64)
-module RT_int64_Dom = STM_Domain.Make(RConf_int64)
-
-module RConf_int_GC = STM.AddGC(RConf_int)
-module RConf_int64_GC = STM.AddGC(RConf_int64)
-
-module RT_int_GC_Seq = STM_Seq.Make(RConf_int_GC)
-module RT_int_GC_Dom = STM_Domain.Make(RConf_int_GC)
-module RT_int64_GC_Seq = STM_Seq.Make(RConf_int64_GC)
-module RT_int64_GC_Dom = STM_Domain.Make(RConf_int64_GC)
-;;
-Util.set_ci_printing ()
-;;
-QCheck_runner.run_tests_main
-  (let count = 1000 in
-   [RT_int_Seq.agree_test        ~count ~name:"global int ref test";
-    RT_int_Dom.agree_test_par    ~count ~name:"global int ref test";
-    RT_int_GC_Dom.agree_test_par ~count ~name:"global int ref test (w/AddGC functor)";
-    RT_int_Seq.agree_test        ~count ~name:"global int64 ref test";
-    RT_int_Dom.agree_test_par    ~count ~name:"global int64 ref test";
-    RT_int_GC_Dom.agree_test_par ~count ~name:"global int64 ref test (w/AddGC functor)";
-   ])
