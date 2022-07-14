755680a046a1bc611fb3bdac49deaa534a91ba62
Jan Midtgaard <mail@janmidtgaard.dk>
2022-07-14 17:40:46 +0200

    clean-up lin_tests

--- src/bytes/lin_tests.ml
+++ src/bytes/lin_tests.ml
@@ -1,71 +1,22 @@
-
-module Bytes_test = struct
-type t = Bytes.t
-
-(* external get : bytes -> int -> char = "get_from_runtime" *)
-
-let get = Bytes.get
- 
-end
-
-module TestConf = struct
-type t = Bytes_test.t
-
-let init () = Stdlib.Bytes.make 42 '0'
-let cleanup _ = ()
-
-open Lin_api
-let api =
-  [ val_ "Bytes_test.get" Bytes_test.get (t @-> int @-> returning_or_exc char)
-]
-
-end
-
-module BConf = struct (*this works and passes all of them ... perhaps because
-                      lin never actually indexes into the bytes?*)
+(* ********************************************************************** *)
+(*                      Tests of thread-unsafe [Bytes]                    *)
+(* ********************************************************************** *)
+module BConf = struct
   type t = Bytes.t
   let init () = Stdlib.Bytes.make 42 '0'
   let cleanup _ = ()
 
   open Lin_api
-  let api =
-    [
-      val_ "Bytes.sub_string" Bytes.sub_string (t @-> int @-> int @-> returning_or_exc string);
-      val_ "Bytes.length" Bytes.length (t @-> returning int);
-      val_ "Bytes.fill" Bytes.fill (t @-> int @-> int @-> char @-> returning_or_exc unit);
-      val_ "Bytes.blit_string" Bytes.blit_string
-        (string @-> int @-> t @-> int @-> int @-> returning_or_exc unit);
-      val_ "Bytes.index_from" Bytes.index_from (t @-> int @-> char @-> returning_or_exc int)]
+  let api = [
+    val_ "Bytes.get"         Bytes.get         (t @-> int @-> returning_or_exc char);
+    val_ "Bytes.sub_string"  Bytes.sub_string  (t @-> int @-> int @-> returning_or_exc string);
+    val_ "Bytes.length"      Bytes.length      (t @-> returning int);
+    val_ "Bytes.fill"        Bytes.fill        (t @-> int @-> int @-> char @-> returning_or_exc unit);
+    val_ "Bytes.blit_string" Bytes.blit_string (string @-> int @-> t @-> int @-> int @-> returning_or_exc unit);
+    val_ "Bytes.index_from"  Bytes.index_from  (t @-> int @-> char @-> returning_or_exc int)]
 end
 
-module BConfGet = struct (*this does not work*)
-  type t = Bytes.t
-  let init () = Stdlib.Bytes.make 42 '0'
-  let cleanup _ = ()
-
-  open Lin_api
-  let api =
-    [
-      val_ "Bytes.unsafe_get" Bytes.unsafe_get (t @-> int @-> returning_or_exc char)
-    ]
-end
-
-
-module BConfSet = struct (*this does not work*)
-  type t = Bytes.t
-  let init () = Stdlib.Bytes.make 42 '0'
-  let cleanup _ = ()
-
-  open Lin_api
-  let api =
-    [
-      val_ "Bytes.set" Bytes.set
-        (t @-> int @-> char @-> returning_or_exc unit)
-    ]
-end
-
-
-module BT = Lin_api.Make(TestConf)
+module BT = Lin_api.Make(BConf)
 ;;
 Util.set_ci_printing ()
 ;;
